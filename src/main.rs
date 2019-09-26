fad3a0b9633cb03fcd7841ce4e2fe89d966898b3
lin pingchuan <paul_lin@foxmail.com>
2019-09-26 23:03:53 +0800

    增加队列阅读形式

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    println!("Hello, world!");
-    let hello="world";
-    println!("Hello, {}",hello);
+    let version="0.0.1";
+    println!("pingchuan-mq [{}] starting now...", version);
+    read_wal_log();
+
+    accept();
+}
+
+fn read_wal_log(){
+    println!("try recovery from WAL")
+}
+
+fn accept(){
+
 }
