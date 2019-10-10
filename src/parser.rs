d7b72fc84b42db373ea3e5c599f979b59f8cba50
lin pingchuan <paul_lin@foxmail.com>
2019-10-10 13:58:18 +0800

    抽象byte成比特数组

--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,19 @@ pub struct PingchuanParser {}
 pub struct PingchuanEvent {
     request_content: String,
 }
+#[derive(Debug)]
+// #[repr(transparent)]
+pub struct PingchuanPacket{
+    transaction_id:u64,
+    topic_len:u64,
+    content_len:u64, 
+    role:u64 ,
+    order:u64, 
+    gzip:u64,
+    offset:u64,
+    topic:String,
+    content:Vec<u8>, 
+}
 impl PingchuanParser {
     pub fn parse(&mut self, request_content: String) -> PingchuanEvent {
         let pingchuan_event = PingchuanEvent::of(request_content);
@@ -19,28 +32,35 @@ impl PingchuanParser {
         std::slice::from_raw_parts((p as *const T) as *const u8, ::std::mem::size_of::<T>())
     }
 
-    pub fn serialize(bytes: &mut [u8]) -> &[u8] {
+    fn add_u64_to_bytes(t:u64,bytes:&mut [u8],offset:usize)->(&[u8],usize){
+        let mut capacity=offset;
+        unsafe {
+            
+            let transaction_id_bytes = std::mem::transmute::<u64, [u8; 8]>(t);
+            for index in 0..transaction_id_bytes.len() {
+                bytes[capacity + index] = transaction_id_bytes[index];
+            }
+            capacity += transaction_id_bytes.len();
+            assert_eq!(
+                bytes[offset..offset + transaction_id_bytes.len()],
+                transaction_id_bytes[..transaction_id_bytes.len()]
+            );
+        }
+        (bytes,capacity)
+    }
+
+    pub fn serialize_to_pingchuan_packet(bytes: &mut [u8]) -> (&[u8],usize) {
         // let  mut bytes:[u8;512]=[0;512];
         let magic_bytes = b"pingchuan";
-        let mut capacity = 0;
+        let mut capacity:usize = 0;
         for index in 0..magic_bytes.len() {
             bytes[index] = magic_bytes[index];
         }
         capacity += magic_bytes.len();
         assert_eq!(bytes[..magic_bytes.len()], magic_bytes[..magic_bytes.len()]);
         let transaction_id: u64 = 13534044911;
-        unsafe {
-            let transaction_id_bytes = std::mem::transmute::<u64, [u8; 8]>(transaction_id);
-            for index in 0..transaction_id_bytes.len() {
-                bytes[capacity + index] = transaction_id_bytes[index];
-            }
-            capacity += transaction_id_bytes.len();
-            assert_eq!(
-                bytes[magic_bytes.len()..magic_bytes.len() + transaction_id_bytes.len()],
-                transaction_id_bytes[..transaction_id_bytes.len()]
-            );
-        }
-        bytes
+        let result=PingchuanParser::add_u64_to_bytes(transaction_id, bytes, capacity);
+        result
     }
 }
 
